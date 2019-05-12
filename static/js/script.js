69ef6b4157b0af7786af2e6f2edf429f35be8f49
shoey-the-shoe <alexvisontay@gmail.com>
2019-05-12 14:55:53 +1000

    added function stubs to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,36 @@
+var products = []
+
 
 $(document).ready(function(){
 
    $('#message').html("This text has been inserted using jQuery from /static/js/script.js")
+   getProductList()
+   console.log(products)
 
 })
 
+function getProductList(){
+   const url = '/products'
+   $.get({
+      url: url,
+      success: function (data) {
+         products = data;
+      }
+   })
+}
+
+function postCart(){
+   //TODO
+}
+
+function getCart(){
+   //TODO
+}
+
+function displayCart(){
+   //TODO
+}
+
+function displayProductInfo(){
+   //TODO
+}
