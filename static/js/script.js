c9aaac57f9cb4043f4118ff5a8673150cf4269ff
shoey-the-shoe <alexvisontay@gmail.com>
2019-05-14 19:54:29 +1000

    partial add to cart implementation

--- static/js/script.js
+++ static/js/script.js
@@ -16,7 +16,24 @@
                     //console.log($(this).parent().find('td:first').html())
                     id = $(this).parent().find('td:first').html()
                     console.log(myProducts.list.products[id])
-                    $("#info").html(myProducts.list.products[id].name+' : $'+myProducts.list.products[id].unit_cost + myProducts.list.products[id].description)
+                    $("#info").html(myProducts.list.products[id].name + ' : $' + myProducts.list.products[id].unit_cost + myProducts.list.products[id].description
+                        + "<img src=" + myProducts.list.products[id].image_url + ">" + "<br>" + "<button id=\"remove\"\n" +
+                        "        type=\"button\">\n" +
+                        "    Remove\n" +
+                        "</button>")
+                    $("#info").append(
+                        ' <button id="addToCart"\n' +
+                        '        type="button">\n' +
+                        '    Add to cart\n' +
+                        '</button>'
+                    )
+
+                    $("#addToCart").click(function () {
+                        myCart.postCart(myProducts.list.products[id].id, 1, 1)
+                    });
+                    $("#remove").click(function () {
+                        $("#info").empty()
+                    });
                 })
             })
             $(window).on("dataChangedCart", function () {
@@ -72,8 +89,19 @@
             })
         }
 
-        Cart.prototype.postCart = function (id, quantity, update) {
-            //TODO
+        Cart.prototype.postCart = function (id, q, u) {
+            var self = this
+            $.post({
+                url: self.url,
+                data: {
+                    productid: id,
+                    quantity: q,
+                    update: u,
+                },
+                success: function (response) {
+                    console.log(response)
+                }
+            })
         }
 
         Cart.prototype.displayCart = function () {
